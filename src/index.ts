a4f237276c2eaa8a39783352a84318c7a43c8430
Jérôme Beau <javarome@gmail.com>
2022-06-23 21:54:31 +0200

    Added server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello")
+import {ClientRequest, ServerResponse} from "http"
+
+const http = require("http")
+const host = "localhost"
+const port = 8080
+const requestListener = (req: ClientRequest, res: ServerResponse) => {
+  res.setHeader("Content-Type", "application/json");
+  res.writeHead(200)
+  const message = {
+    message: "grhow"
+  }
+  res.end(JSON.stringify(message))
+}
+const server = http.createServer(requestListener)
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`)
+})
