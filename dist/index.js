eb66332636b8e001a7e83ab21745b19f9edc3f7c
Jérôme Beau <javarome@gmail.com>
2022-07-01 19:39:50 +0200

    Use date

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,24 @@
 const welcomeEl = document.getElementById("welcome")
 const homeEl = document.getElementById("home")
 const serverUrl = "https://grhow-server-4ajpz2roya-ew.a.run.app"
-fetch(serverUrl)
-  .then(function(response) {
-    return response.json();
+const headers = new Headers()
+const date = new Date().toUTCString()
+headers.append("Accept", "application/json")
+headers.append("Content-Type", "application/json")
+let body = JSON.stringify({date})
+fetch(serverUrl, {method: "POST", headers, body})
+  .then(function (response) {
+    let locale = response.headers.get("Content-Language")
+    document.documentElement.lang = locale.substring(0, 2)
+    return response.json()
   })
-  .then(function(json) {
+  .then(function (json) {
     homeEl.textContent = json.message
     welcomeEl.style.animation = "fade 1s ease-out forwards"
     homeEl.style.animation = "fade 1s ease-out reverse forwards"
-    welcomeEl.addEventListener('animationend', (ev) => {
+    welcomeEl.addEventListener("animationend", (ev) => {
       welcomeEl.style.display = "none"
-    });
+    })
   })
   .catch(err => {
     console.error(err)
--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,63 @@
-import {IncomingMessage, RequestListener, ServerResponse} from 'http'
+import {IncomingMessage, RequestListener, ServerResponse} from "http"
 
 interface Messages {
-  hello: string
+  hello(userDate: Date): string
 }
-const allMessages: {[locale: string]: Messages} = {
-  en: { hello: "Hello"},
-  fr: { hello: "Bonjour"},
-  es: { hello: "Hola"},
-  it: { hello: "Ciao"},
-  pt: { hello: "Olá"},
-  mi: { hello: "Kia Ora"},
-  ["en-au"]: { hello: "G’day"},
-  el: { hello: "γεια"},
-  sr: { hello: "Здраво"},
-  hr: { hello: "Zdravo"},
-  ru: { hello: "привет"},
-  hi: { hello: "नमस्ते"},
-  jp: { hello: "こんにちは"},
-  tr: { hello: "Merhaba"},
-  ko: { hello: "안녕하세요"},
-  sk: { hello: "Ahoj"},
-  de: { hello: "Guten tag"},
-  nl: { hello: "Hallo"},
-  po: { hello: "Cześć"},
+
+const allMessages: { [locale: string]: Messages } = {
+  en: {
+    hello: (userDate: Date) => {
+      return userDate.getHours() > 18 ? "Good evening" : userDate.getHours() < 11 ? "Good morning" : "Hello"
+    }
+  },
+  fr: {
+    hello: (userDate: Date) => {
+      return userDate.getHours() > 18 ? "Bonsoir" : "Bonjour"
+    }
+  },
+  es: {hello: (_userDate: Date) => "Hola"},
+  it: {hello: (_userDate: Date) => "Ciao"},
+  pt: {hello: (_userDate: Date) => "Olá"},
+  mi: {hello: (_userDate: Date) => "Kia Ora"},
+  ["en-au"]: {hello: (_userDate: Date) => "G’day"},
+  el: {hello: (_userDate: Date) => "γεια"},
+  sr: {hello: (_userDate: Date) => "Здраво"},
+  hr: {hello: (_userDate: Date) => "Zdravo"},
+  ru: {hello: (_userDate: Date) => "привет"},
+  hi: {hello: (_userDate: Date) => "नमस्ते"},
+  jp: {hello: (_userDate: Date) => "こんにちは"},
+  tr: {hello: (_userDate: Date) => "Merhaba"},
+  ko: {hello: (_userDate: Date) => "안녕하세요"},
+  sk: {hello: (_userDate: Date) => "Ahoj"},
+  de: {hello: (_userDate: Date) => "Guten tag"},
+  nl: {hello: (_userDate: Date) => "Hallo"},
+  po: {hello: (_userDate: Date) => "Cześć"}
 }
-export const requestListener: RequestListener = (req: IncomingMessage, res: ServerResponse): void => {
-  let langHeader = req.headers['accept-language'] || ""
-  const langs = langHeader.split(",")
-  const lg = langs.map(lang => lang.split(";")[0])
-  const locale = (lg[0] || "en").toLowerCase();
-  const messages = allMessages[locale] || allMessages[locale.split("-")[0]]
-  let front = "*" // https://grhow.com"
-  res.setHeader('Access-Control-Allow-Origin', front)
-  res.setHeader('Content-Type', 'application/json')
-  res.writeHead(200)
-  res.end(JSON.stringify({message: messages.hello}))
+export const requestListener: RequestListener = async (req: IncomingMessage, res: ServerResponse): Promise<void> => {
+  let payload: any
+  if (req.method === "POST") {
+    let langHeader = req.headers["accept-language"] || ""
+    const langs = langHeader.split(",")
+    const lg = langs.map(lang => lang.split(";")[0])
+    const locale = (lg[0] || "en").toLowerCase()
+    const buffers = []
+    for await (const chunk of req) {
+      buffers.push(chunk)
+    }
+    const data = Buffer.concat(buffers).toString()
+    const json = JSON.parse(data)
+    const dateHeader = req.headers["date"] as string
+    const userDate = json.date ? new Date(json.date) : dateHeader ? new Date(dateHeader) : new Date()
+    const messages = allMessages[locale] || allMessages[locale.split("-")[0]]
+    res.setHeader("Content-Type", "application/json")
+    res.setHeader("Content-Language", locale)
+    payload = JSON.stringify({message: messages.hello(userDate)})
+  }
+  const allowedOrigins = "https://grhow.com"
+  res.setHeader("Access-Control-Allow-Methods", "POST")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept")
+  res.setHeader("Access-Control-Allow-Origin", allowedOrigins)
+  res.writeHead(200);
+  res.end(payload);
 }
 
